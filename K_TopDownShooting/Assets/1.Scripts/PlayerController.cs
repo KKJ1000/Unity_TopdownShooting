7e6ccc693a00d13e87f312a9a403f08e54d0e4d0
KKJ1000 <kwangjoon1733@naver.com>
2024-12-20 23:15:15 +0900

    [Feat] Main, GameOver Scene 구현

--- K_TopDownShooting/Assets/1.Scripts/PlayerController.cs
+++ K_TopDownShooting/Assets/1.Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -118,6 +119,6 @@ public class PlayerController : MonoBehaviour
 
     void AfterDying()
     {
-        //gameObject.SetActive(false);
+        SceneManager.LoadScene("GameOverScene");
     }
 }
